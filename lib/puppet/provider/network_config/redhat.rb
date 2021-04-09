739a03a673cd5877d369d99cabff9fc13ef3021d
Seth Tunstall <seth@tunstall.in>
2021-04-09 12:19:31 +0100

    pairs hash sort

--- lib/puppet/provider/network_config/redhat.rb
+++ lib/puppet/provider/network_config/redhat.rb
@@ -230,7 +230,7 @@ Puppet::Type.type(:network_config).provide(:redhat) do
       pairs[key] = %("#{val}") if val.is_a?(String) && val.match(%r{\s+})
     end
 
-    pairs.sort!
+    pairs = pairs.sort.to_h
     pairs
   end
 
